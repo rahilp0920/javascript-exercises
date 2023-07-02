01898b97a537ebd986160cd949f21fb73860f693
rahilp0920 <120121015+rahilp0920@users.noreply.github.com>
2023-07-01 21:44:37 -0400

    Update removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,...args) {
+  let newArray=[];
+  array.forEach(item =>{
 
+    if!(item in args){
+
+      newArray.push(item);
+    }
+
+  })
+
+  return newArray;
 };
 
 // Do not edit below this line
