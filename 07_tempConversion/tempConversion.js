8f3f3f6d54824dcc22aecfb1eff0ab096febf2b7
rahilp0920 <120121015+rahilp0920@users.noreply.github.com>
2023-07-01 22:02:12 -0400

    Update tempConversion.js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(a) {
+
+  num=(a-32)*(5/9);
+  return parseInt(num.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(a) {
+
+  num = (a*9/5)+32;
+  return parseInt(num.toFixed(1));
+
 };
 
-// Do not edit below this line
-module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
-};
